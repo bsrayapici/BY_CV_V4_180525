fb4032ab45b098b14c1e047103e2e6cacbe9da20
bsrayapici <bsrayapici95@gmail.com>
2025-05-25 14:05:25 +0300

    Feature : added local storage for theme & language preferences

--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -1,10 +1,18 @@
 import { createContext, useContext, useState } from "react";
+import { useEffect } from "react";
+
 
 // Context
 const LanguageContext = createContext();
 
 export const LanguageProvider = ({children}) => {
-  const [language, setLanguage] = useState("en");
+  const [language, setLanguage] = useState(() => {
+    return localStorage.getItem("language") || "en";
+  });
+
+  useEffect(() => {
+  localStorage.setItem("language", language);
+}, [language]);
 
   return (
     <LanguageContext.Provider value={{language, setLanguage}}>
--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -3,18 +3,25 @@ import { createContext, useContext, useState, useEffect } from 'react';
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
-
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedValue = localStorage.getItem('theme');
+    return storedValue === 'dark';
+  });
   useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
     } else {
       document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
     }
   }, [darkMode]);
 
+  //const toggleTheme = () => {
+    //setDarkMode(!darkMode);
+  //};
   const toggleTheme = () => {
-    setDarkMode(!darkMode);
+    setDarkMode(prev => !prev);
   };
 
   return (
