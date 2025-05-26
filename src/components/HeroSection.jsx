eebe34da46e46acc20a3bcaf23bfad5e22ab083e
busra <37024872+bsrayapici@users.noreply.github.com>
2025-05-26 17:43:02 +0300

    Restore local image paths

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,6 +3,8 @@ import { useTheme } from '../contexts/ThemeContext';
 import { useLanguage } from '../contexts/LanguageContext';
 import { data } from '../constants/data';
 
+import ellipse10 from '../assets/Ellipse 10.png';
+
 const HeroSection = () => {
   const { language } = useLanguage();
   const { theme } = useTheme(); // dark veya light
@@ -18,9 +20,11 @@ const HeroSection = () => {
       ></div>
 
       {/* 🎯 Decorative circle top-left */}
-      <div
+      <img
+        src={ellipse10}
+        alt=""
         aria-hidden="true"
-        className="hidden md:block absolute w-[160px] h-[120px] left-[421px] top-[-45px] bg-[#93d0f4] rounded-full opacity-50"
+        className="hidden md:block absolute w-[160px] h-[120px] left-[421px] top-[-45px]"
       />
 
       <div className="flex flex-col lg:flex-row items-center justify-between max-w-7xl mx-auto gap-12">
